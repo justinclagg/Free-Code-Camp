ee327cea443ef903e3e5cbebed704ff96c2dafd8
Justin Clagg <justinclagg@gmail.com>
2015-12-30 18:35:24 -0500

    added 06 bonfire

---
+++ Basic Algorithm Scripting/06 Bonfire Title Case a Sentence.js
@@ -0,0 +1,10 @@
+/* Bonfire: Title Case a Sentence
+http://www.freecodecamp.com/challenges/bonfire-title-case-a-sentence
+
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
+Code by Justin Clagg
+https://github.com/justinclagg
+*/
+
