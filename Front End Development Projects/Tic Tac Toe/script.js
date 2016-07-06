1d9f54534ef5d4e14aa6f5d775859969a560f136
Justin Clagg <justinclagg@gmail.com>
2016-07-06 19:00:41 -0400

    updated Tic Tac Toe

--- Front End Development Projects/Tic Tac Toe/script.js
+++ Front End Development Projects/Tic Tac Toe/script.js
@@ -1,117 +1,119 @@
 "use strict";
 
 $(document).ready(function () {
-  
-  var userSymbol = "X";
-  var compSymbol = "O";
-  var difficulty = "Easy";
-  var squareArr = document.getElementsByClassName("square");
-  var board = ["E", "E", "E", "E", "E", "E", "E", "E", "E"];
-  var state = "";
-  
-  // Changes the symbol
-  $(".symbol-buttons > .btn").click(function() {
+
+  var gameState = {
+    "userSymbol": "X",
+    "compSymbol": "O",
+    "difficulty": "Easy",
+    "squareNodes": document.getElementsByClassName("square")
+  };
+
+  // Symbol change
+  $(".symbol-buttons > button").click(function changeSymbol() {
+    var tempSymbol = gameState.compSymbol;
+    gameState.compSymbol = gameState.userSymbol;
+    gameState.userSymbol = tempSymbol;
     $(this).addClass("active");
     $(this).siblings().removeClass("active");
-    compSymbol = userSymbol;
-    userSymbol = $(this).html();
   });
   
-  // Changes the difficulty level
-  $(".difficulty-buttons > .btn").click(function() {
+  // Difficulty changes
+  $(".difficulty-buttons > button").click(function changeDifficulty() {
+    gameState.difficulty = $(this).text();
     $(this).addClass("active");
     $(this).siblings().removeClass("active");
-    difficulty = $(this).html();
+  });
+
+  // User move, initiated by square click
+  $(".square").click(function userMove() {
+    if ($(this).html() === "") {
+      $(this).html(gameState.userSymbol);
+      if (checkTerminal() === false) {
+        // Delayed computer moves
+        setTimeout(compMove, 500);
+        setTimeout(checkTerminal, 500);
+      }
+    }
   });
   
   function compMove(state) {
-    // Computer places in random squares
-    if (difficulty == "Easy") {
+    // Easy - computer places in random squares
+    if (gameState.difficulty == "Easy") {
       for (var i = 0; i < 9; i++) {
-        if (!squareArr[i].innerHTML) {
-          squareArr[i].innerHTML = compSymbol;
-          board[i] = 0;
-          return;
+        if (gameState.squareNodes[i].innerHTML === "") {
+          gameState.squareNodes[i].innerHTML = gameState.compSymbol;
+          return; // End computer move after first empty square is found
         }
       }
-    } else if (difficulty == "Medium") {
+    // Medium - 
+    // } else if (gameState.difficulty == "Medium") {
     
-    // User can at best tie
-    } else if (difficulty == "Hard") {
+    // Hard - user can at best tie the computer
+    // } else if (gameState.difficulty == "Hard") {
       
     }
   }
   
-  // Update the board array, representing user entries with 1,
-  // and computer entries with 0
-  function updateBoard() {
-    for (var i = 0; i < 9; i++) {
-      if (squareArr[i].innerHTML) {
-        board[i] = squareArr[i].innerHTML === userSymbol ? 1 : 0;
-      }
-    }
-  }
-  
-  function newGame() {
-    board = ["E", "E", "E", "E", "E", "E", "E", "E", "E"];
-    for (var i = 0; i < 9; i++) {
-      squareArr[i].innerHTML = "";
-      squareArr[i].classList.remove("winning-square__user");
-      squareArr[i].classList.remove("winning-square__comp");
-    }
-  }
-  
-  // Player can be "User" or "Computer"
-  function win(player, line) {
-    if (player === "User") {
-      for (var i = 0; i < 3; i++) {
-        squareArr[line[i]].classList.add("winning-square__user");
-      }
-    } else {
-      for (var i = 0; i < 3; i++) {
-        squareArr[line[i]].classList.add("winning-square__comp");
-      }
-    }
-    setTimeout(newGame, 1500);
-  }
-  
-  function checkTerminal(state) {
+  function checkTerminal() {
     // Check diagonals for win
-    if (board[0] !== "E" && board[0] === board[4] && board[4] === board[8]) {
-      win(board[0] === 1 ? "User" : "Computer", [0, 4, 8]);
-      return true;
-    } else if (board[2] !== "E" && board[2] === board[4] && board[4] === board[6]) {
-      win(board[2] === 1 ? "User" : "Computer", [2, 4, 6]);
-      return true;
+    if (gameState.squareNodes[0].innerHTML 
+        && gameState.squareNodes[0].innerHTML === gameState.squareNodes[4].innerHTML 
+        && gameState.squareNodes[4].innerHTML === gameState.squareNodes[8].innerHTML) 
+    {
+        win(gameState.squareNodes[0].innerHTML === gameState.userSymbol ? "User" : "Computer", [0, 4, 8]);
+        return true;
+    } 
+    if (gameState.squareNodes[2].innerHTML 
+        && gameState.squareNodes[2].innerHTML === gameState.squareNodes[4].innerHTML 
+        && gameState.squareNodes[4].innerHTML === gameState.squareNodes[6].innerHTML) 
+    {
+        win(gameState.squareNodes[2].innerHTML === gameState.userSymbol ? "User" : "Computer", [2, 4, 6]);
+        return true;
     }
     // Check rows for win
     for (var i = 0; i < 7; i += 3) {
-      if (board[i] !== "E" && board[i] === board[i + 1] && board[i + 1] === board[i + 2]) {
-        win(board[i] === 1 ? "User" : "Computer", [i, i + 1, i + 2]);
-        return true;
+      if (gameState.squareNodes[i].innerHTML 
+          && gameState.squareNodes[i].innerHTML === gameState.squareNodes[i + 1].innerHTML 
+          && gameState.squareNodes[i + 1].innerHTML === gameState.squareNodes[i + 2].innerHTML) 
+      {
+          win(gameState.squareNodes[i].innerHTML === gameState.userSymbol ? "User" : "Computer", [i, i + 1, i + 2]);
+          return true;
       }
     }
     // Check columns for win
     for (var i = 0; i < 3; i++) {
-      if (board[i] !== "E" && board[i] === board[i + 3] && board[i + 3] === board[i + 6]) {
-        win(board[i] === 1 ? "User" : "Computer", [i, i + 3, i + 6]);
-        return true;
+      if (gameState.squareNodes[i].innerHTML 
+          && gameState.squareNodes[i].innerHTML === gameState.squareNodes[i + 3].innerHTML 
+          && gameState.squareNodes[i + 3].innerHTML === gameState.squareNodes[i + 6].innerHTML) 
+      {
+          win(gameState.squareNodes[i].innerHTML === gameState.userSymbol ? "User" : "Computer", [i, i + 3, i + 6]);
+          return true;
       }
     }
-    return false;
+    return false; // No terminal condition
   }
-  
-  $(".square").click(function() {
-    if (!$(this).html()) {
-      $(this).html(userSymbol);
-      updateBoard();
-      if (checkTerminal() === false) {
-        compMove();
-        checkTerminal();
+
+  // Player can be "User" or "Computer"
+  function win(player, line) {
+    if (player === "User") {
+      for (var i = 0; i < 3; i++) {
+        gameState.squareNodes[line[i]].classList.add("winning-square__user");
+      }
+    } else {
+      for (var i = 0; i < 3; i++) {
+        gameState.squareNodes[line[i]].classList.add("winning-square__comp");
       }
     }
-  });
-  
+    setTimeout(newGame, 1500);
+  }
 
-  
+  function newGame() {
+    for (var i = 0; i < 9; i++) {
+      gameState.squareNodes[i].innerHTML = "";
+      gameState.squareNodes[i].classList.remove("winning-square__user");
+      gameState.squareNodes[i].classList.remove("winning-square__comp");
+    }
+  }
+
 });
