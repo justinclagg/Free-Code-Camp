40eae83e9d1a170723d9972086cff3a69f865845
Justin Clagg <justinclagg@gmail.com>
2016-07-17 20:55:17 -0400

    finished Tribute Page

--- Front End Development Projects/Tribute Page/script.js
+++ Front End Development Projects/Tribute Page/script.js
@@ -1,11 +1,11 @@
 /*eslint-env browser */
-
 document.addEventListener("DOMContentLoaded", function() {
 // Starts script when HTML is loaded and parsed; Not supported before IE9
 	"use strict";
 
 	/* Document status variables */
 	var menuRaised = false,
+		activeTab = false,
 		activePanel = false,
 		activeDropdown = "physics-dropdown",
 		activeIcon = "physics";
@@ -21,21 +21,26 @@ document.addEventListener("DOMContentLoaded", function() {
 	// 'this' is the element that was clicked
 	function tabClick() {
 		
-		if (menuRaised) {
-			// Remove the previously active panel, then update and display the new active panel
-			document.getElementById(activePanel).classList.remove("active-panel");
-			activePanel = this.id + "-panel";
-			document.getElementById(activePanel).classList.add("active-panel");
-		}
-		else {
+		if (menuRaised === false) {
 			// The first tab click raises the menu
 			document.getElementById("menu").classList.add("menu-raised");			
 			menuRaised = true;
+			this.classList.add("active-tab");
+			activeTab = this.id;
 			// Set the active panel and display it after a delay
 			activePanel = this.id + "-panel";
 			setTimeout(function displayPanel() {
 				document.getElementById(activePanel).classList.add("active-panel");
 			}, 1200);
+		}
+		else if (this.id !== activeTab){
+			// Remove the previously active panel, then update and display the new active panel
+			document.getElementById(activePanel).classList.remove("active-panel");
+			activePanel = this.id + "-panel";
+			document.getElementById(activePanel).classList.add("active-panel");
+			document.getElementById(activeTab).classList.remove("active-tab");
+			activeTab = this.id;
+			this.classList.add("active-tab");
 		}
 	}
 
