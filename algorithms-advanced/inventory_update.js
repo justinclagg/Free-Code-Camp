9cd0de10183aac8c2b99b2cb35dea65a69fa4500
Justin Clagg <justinclagg@gmail.com>
2017-01-09 01:12:28 -0500

    description

--- algorithms-advanced/inventory_update.js
+++ algorithms-advanced/inventory_update.js
@@ -1,3 +1,15 @@
+/* 
+Inventory Update
+https://www.freecodecamp.com/challenges/inventory-update
+
+Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. 
+Update the current existing inventory item quantities (in arr1). If an item cannot be found, 
+add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
+
+Code by Justin Clagg
+https://github.com/justinclagg
+*/
+
 function updateInventory(oldInv, newInv) {
 	const hash = hash2DArray(oldInv);
 	for (let i = 0; i < newInv.length; i++) {
